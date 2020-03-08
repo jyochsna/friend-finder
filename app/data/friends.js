987917f931721a85504bc9a7729235163ff46489
jyochsna <55848802+jyochsna@users.noreply.github.com>
2020-03-07 21:48:13 -0800

    friend.js updated

---
+++ app/data/friends.js
@@ -0,0 +1,92 @@
+
+var friendsArray = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+
+      {
+        "name":"pooja",
+        "photo":"pooja.jpg",
+        "scores":[
+            2,
+            1,
+            5,
+            4,
+            2,
+            2,
+            3,
+            1,
+            5,
+            2
+          ]
+      },
+
+      {
+        "name":"trisna",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            3,
+            1,
+            4,
+            2,
+            5,
+            1,
+            5,
+            2,
+            3,
+            4
+          ]
+      },
+
+      {
+        "name":"kanji",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,
+            2,
+            3,
+            4,
+            5,
+            4,
+            5,
+            2,
+            1,
+            3
+            
+          ]
+      },
+
+      {
+        "name":"rose",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,
+            5,
+            3,
+            5,
+            2,
+            4,
+            1,
+            4,
+            5,
+            3
+            
+        
+          ]
+      }
+];
+
+module.exports = friendsArray;
